770d7c472e7526b5fd014e5acee040c2dd5d3ba1
Saren <ykmran@gmail.com>
2014-04-15 09:06:10 +0800

    Less hardcoding.

--- src/main/java/net/wtako/SILOT2/Commands/silot2/ArgHelp.java
+++ src/main/java/net/wtako/SILOT2/Commands/silot2/ArgHelp.java
@@ -1,5 +1,6 @@
 package net.wtako.SILOT2.Commands.silot2;
 
+import net.wtako.SILOT2.Main;
 import net.wtako.SILOT2.Utils.Lang;
 
 import org.bukkit.command.CommandSender;
@@ -7,8 +8,8 @@ import org.bukkit.command.CommandSender;
 public class ArgHelp {
 
     public ArgHelp(CommandSender sender) {
-        sender.sendMessage("SimpleItemLottery2 v0.0.1");
-        sender.sendMessage("Author: Saren");
+        sender.sendMessage(Main.getInstance().getName() + " v" + Main.getInstance().getProperty("version"));
+        sender.sendMessage("Author: " + Main.getInstance().getProperty("author"));
         sender.sendMessage(Lang.HELP_ADD.toString());
         sender.sendMessage(Lang.HELP_LIST.toString());
         sender.sendMessage(Lang.HELP_MAKE.toString());
